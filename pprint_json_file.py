43c37e3bfb9de39affeb1954fbc79cd60ce5ab7d
EvgeniyRoss <33811445+EvgeniyRoss@users.noreply.github.com>
2017-11-28 11:55:03 +0300

    Update pprint_json_file.py

--- pprint_json_file.py
+++ pprint_json_file.py
@@ -2,12 +2,14 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data(r'''C:\Users\Ползователь\4_json\alco_shops.json'''))
